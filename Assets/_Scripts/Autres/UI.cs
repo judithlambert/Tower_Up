0aba221773ab5d79f7eb2190cb9cfcc5033f1b94
AdamPrevost <35999571+AdamPrevost@users.noreply.github.com>
2018-02-21 16:37:20 -0500

    rien

---
+++ Assets/_Scripts/Autres/UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    Text temps;
+    char[] trim = new char[] { '.' };
+
+	void Awake ()
+    {
+        temps = GetComponentInChildren<Text>();
+	}
+	
+	void Update ()
+    {
+        temps.text = ((int)(Time.time / 60)).ToString() + ":" + ((int)(Time.time % 60)).ToString("00") + ":" + Time.time.ToString().Split(trim).Last();
+	}
+}
