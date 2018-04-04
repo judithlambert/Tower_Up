040e2788fde616aba815bc2012fd1b306f0a95fa
AdamPrevost <35999571+AdamPrevost@users.noreply.github.com>
2018-04-04 11:15:41 -0400

    étage boss + projectile

---
+++ Assets/_Scripts/ÉtageBoss.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ÉtageBoss : MonoBehaviour
+{
+    float deltaTemps;
+
+    void Update()
+    {
+        deltaTemps += Time.deltaTime;
+        if (deltaTemps >= 2)
+        {
+            Instantiate(Resources.Load<GameObject>("Prefabs/Projectile"), new Vector3(0, 6, 0), Quaternion.identity);
+            deltaTemps = 0;
+        }
+    }
+}
